9742ace1bd5073947b9c983ba18aa594fc494f41
jaquelinecontini <jaquelinecontini0@gmail.com>
2023-06-19 12:39:09 -0300

    second commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+function getDistanceFromTheTop(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
+  smoothScrollTo(0, distanceFromTheTop);
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
+
